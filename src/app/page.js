033585380bfb3c137b8e1dc72e760fc4da890db8
Michał Jodko <166276@student.uwm.edu.pl>
2025-03-20 12:30:35 +0100

    MyMlodzi Nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,95 @@
-import Image from "next/image";
-
+"use client";
+import "./globals.css";
+import Autoplay from "embla-carousel-autoplay"
+import Checkout from './components/checkout'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from 'next/image';
+import TextFiles from './components/TextFiles';
 export default function Home() {
+  const photos = [
+    "/slide1.png",
+    "/slide2.png",
+    "/slide3.png",
+    "/slide4.png",
+    "/slide5.png",
+    "/slide6.png",
+    "/slide7.png",
+  ]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <main>
+        <div className="flex flex-col lg:flex-row h-screen">
+          <div className="relative min-w-[70%] min-h-full bg-cover">
+            <div
+              className="relative min-h-full overflow-hidden min-w-full bg-cover bg-center block"
+            >
+              <Carousel
+              // plugins={[
+              //   Autoplay({
+              //     delay: 15000,
+              //   }),
+              // ]}
+              opts={{
+                align: "start",
+                loop: true,
+              }}
+              >
+                <CarouselContent>
+                  {photos.map((photo, i) => (
+                    <CarouselItem key={i}>
+                      <div className="relative w-full h-full aspect-auto min-h-[100vh]">
+                        <Image
+                          alt="Opis działań fundacji"
+                          src={photo}
+                          quality={100}
+                          fill
+                          sizes="100vw"
+                          className={`object-cover ${i === 1 || i === 4 ? 'object-right' : 'object-center'}`}
+                          priority
+                        />
+                      <TextFiles index={i}/>
+                      </div>
+                  </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="z-20"/>
+                <CarouselNext className="z-20"/>
+              </Carousel>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {/* <div v-show="isMobile" className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50">
+              <div v-show="showScrollButton">
+                <button
+                  @click="scrollToBottom"
+                  className="p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-700 focus:outline-none"
+                >
+                  <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                  </svg>
+                </button>
+              </div>
+            </div> */}
+          </div>
+
+          <div id="checkout" className="flex flex-grow flex-col text-center items-center p-7 bg-[#2b3e95]">
+            <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10">Współtwórz</h1>
+            <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10 mb-5">z nami przyszłość!</h1>
+            <p className="text-base md:text-3xl lg:text-[1.4vw] text-white justify-between text-wrap p-4">
+              Postaw na jakość i skuteczność. <strong>Twoja darowizna pozwala nam tworzyć programy i inicjatywy, które zmieniają życie mieszkańców.</strong> Razem możemy działać jeszcze skutecznie j i odpowiadać na potrzeby lokalnych społeczności. Wspólnie możemy więcej!
+            </p>
+            <br/>
+            <div className="bg-white rounded-lg overflow-scroll">
+              <Checkout />
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
