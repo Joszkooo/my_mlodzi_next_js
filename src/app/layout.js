4b87549707bf79f0f3527962fe5fc5d7ba9d9c90
Michał Jodko <166276@student.uwm.edu.pl>
2025-03-28 00:04:21 +0100

    css changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,16 @@ export const metadata = {
   description: "Fundacja My Mlodzi",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${poppins.variable} antialiased`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import "./globals.css";
 import Autoplay from "embla-carousel-autoplay"
-import Checkout from './components/checkout'
+// import Checkout from './components/checkout'
 import {
   Carousel,
   CarouselContent,
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/carousel"
 import Image from 'next/image';
 import TextFiles from './components/TextFiles';
+
 export default function Home() {
   const photos = [
     "/slide1.png",
@@ -21,75 +22,73 @@ export default function Home() {
     "/slide6.png",
     "/slide7.png",
   ]
+
   return (
     <>
-      <main>
-        <div className="flex flex-col lg:flex-row h-screen">
-          <div className="relative min-w-[70%] bg-contain">
-            <div
-              className="relative min-h-full overflow-hidden  min-w-full bg-cover bg-center block"
-            >
-              <Carousel
-              // plugins={[
-              //   Autoplay({
-              //     delay: 15000,
-              //   }),
-              // ]}
-              opts={{
-                align: "start",
-                loop: true,
-              }}
+      <main className="flex flex-col lg:flex-row h-screen">
+        <div className="min-w-[70%] relative">
+          <Carousel
+          // plugins={[
+          //   Autoplay({
+          //     delay: 15000,
+          //   }),
+          // ]}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          >
+            <CarouselContent className="h-full">
+              {photos.map((photo, i) => (
+                <CarouselItem key={i} className="h-[100vh]">
+                  <div className={`relative grid ${i === 1 || i === 4 ? 'grid-cols-2' : 'grid-rows-2'} h-full`}>
+                    
+                    <div className="relative p-2 overflow-y-auto z-10">
+                      <TextFiles index={i} />
+                    </div>
+
+                    <div className="relative w-full h-full">
+                      <Image
+                        alt="Opis działań fundacji"
+                        src={photo}
+                        fill
+                        // jak sie nie bedzie podobac to mozna zmienic na object-contain
+                        className={`object-contain ${i === 1 || i === 4 ? 'object-right' : 'object-bottom'} `}
+                        priority
+                        sizes="(max-width: 2048px) 100vw, 100vw"
+                      />
+                    </div>
+                  </div>
+              </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="z-100 left-2"/>
+            <CarouselNext className="z-100 right-2"/>
+          </Carousel>
+
+          <div v-show="isMobile" className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50">
+            <div v-show="showScrollButton">
+              <button
+                onClick="scrollToBottom"
+                className="p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-700 focus:outline-none"
               >
-                <CarouselContent>
-                  {photos.map((photo, i) => (
-                    <CarouselItem key={i}>
-                      <div className={`relative grid ${i === 1 || i === 4 ? 'grid-cols-2' : 'grid-rows-2'} min-h-[100vh]`}>
-                        <div className="">
-                          <TextFiles index={i}/>
-                        </div>
-                        <div className="aspect-auto">
-                          <Image
-                            alt="Opis działań fundacji"
-                            src={photo}
-                            // quality={100}
-                            fill
-                            className={`object-contain ${i === 1 || i === 4 ? 'object-right' : 'object-bottom'} `}
-                            priority
-                          />
-                        </div>
-                      </div>
-                  </CarouselItem>
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious className="z-20"/>
-                <CarouselNext className="z-20"/>
-              </Carousel>
+                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
             </div>
-
-            {/* <div v-show="isMobile" className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50">
-              <div v-show="showScrollButton">
-                <button
-                  @click="scrollToBottom"
-                  className="p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-700 focus:outline-none"
-                >
-                  <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                  </svg>
-                </button>
-              </div>
-            </div> */}
           </div>
+        </div>
 
-          <div id="checkout" className="flex flex-grow flex-col text-center items-center p-7 bg-[#2b3e95]">
-            <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10">Współtwórz</h1>
-            <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10 mb-5">z nami przyszłość!</h1>
-            <p className="text-base md:text-3xl lg:text-[1.4vw] text-white justify-between text-wrap p-4">
-              Postaw na jakość i skuteczność. <strong>Twoja darowizna pozwala nam tworzyć programy i inicjatywy, które zmieniają życie mieszkańców.</strong> Razem możemy działać jeszcze skutecznie j i odpowiadać na potrzeby lokalnych społeczności. Wspólnie możemy więcej!
-            </p>
-            <br/>
-            <div className="bg-white rounded-lg overflow-scroll">
-              <Checkout />
-            </div>
+        <div id="checkout" className="flex flex-grow flex-col text-center items-center p-7 bg-[#2b3e95]">
+          <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10">Współtwórz</h1>
+          <h1 className="text-4xl md:text-6xl lg:text-[3vw] font-bold text-white uppercase px-10 mb-5">z nami przyszłość!</h1>
+          <p className="text-base md:text-3xl lg:text-[1.4vw] text-white justify-between text-wrap p-4">
+            Postaw na jakość i skuteczność. <strong>Twoja darowizna pozwala nam tworzyć programy i inicjatywy, które zmieniają życie mieszkańców.</strong> Razem możemy działać jeszcze skutecznie j i odpowiadać na potrzeby lokalnych społeczności. Wspólnie możemy więcej!
+          </p>
+          <br/>
+          <div className="bg-white rounded-lg overflow-scroll">
+            {/* <Checkout /> */}
           </div>
         </div>
       </main>
