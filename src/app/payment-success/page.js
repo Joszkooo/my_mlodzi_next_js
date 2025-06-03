de23dcb94be4c50749399e3ec24dd7ecd77f0ea4
Michał Jodko <166276@student.uwm.edu.pl>
2025-06-03 20:10:52 +0200

    Fixed bug with suspense for build process

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -1,45 +1,10 @@
-'use client'
+import { Suspense } from "react";
+import PaymentSuccess from "@/components/PaymentSuccess";
 
-import { useSearchParams } from "next/navigation";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-
-export default function PaymentSuccess() {
-  const searchParams = useSearchParams();
-  const amount = searchParams.get("amount");
-  
+export default function PaymentSuccessPage() {
   return (
-    <section>
-      <Image
-        alt="Ręce wyciągające prezenty w ramach podziękowań"
-        src="/complete.png"
-        fill
-        priority
-        className="filter blur-sm md:blur-none"
-      />
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="text-white text-center px-4">
-          <p className="font-bold text-3xl md:text-6xl">
-            DZIĘKUJEMY ZA WSPARCIE, <br />
-            DZIĘKUJEMY, ŻE JESTEŚ! <br />
-            WPŁACONO {amount} zł!
-          </p>
-
-          <p className="mt-5">
-            Jeśli masz jakieś pytania, proszę napisz{" "}
-            <a href="mailto:mymlodzi@gmail.com" className="underline">
-              mymlodzi@gmail.com
-            </a>
-          </p>
-
-          <Link href="/" passHref>
-            <Button variant="secondary" className="mt-5">
-              Powrót do strony głównej
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </section>
+    <Suspense fallback={<div>Ładowanie danych płatności...</div>}>
+      <PaymentSuccess />
+    </Suspense>
   );
 }
